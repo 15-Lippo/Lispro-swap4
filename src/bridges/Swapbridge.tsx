5581560322337d054cc6e0f78e2b92925dc23bdb
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-05-17 16:54:54 +0200

    Update Swapbridge.tsx

--- src/bridges/Swapbridge.tsx
+++ src/bridges/Swapbridge.tsx
@@ -1,9 +1,10 @@
 import { LiFiWidget, WidgetConfig } from '@lifi/widget';
 
 const widgetConfig: WidgetConfig = {
-  integrator: 'Lisprocoin',
+  integrator: 'krusty',
   fromChain: 137,
-  fromToken: '0x65Abd48bC38DEA1938AE94428590d4da54f74Fbe',
+  fromToken: '0x386C126Edf2013A0f146BDe8dF1b67D93e2ACE90',
+   logoURI: 'https://raw.githubusercontent.com/Krustycoin0/swap/main/src/components/navbar/logoURI.jpg',
   containerStyle: {
     border: '1px solid rgb(234, 234, 234)',
     borderRadius: '16px',
