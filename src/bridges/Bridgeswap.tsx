fffb62e045af4fcc4d175fce4388c8e2256f244c
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2025-01-12 00:55:14 +0100

    Update Bridgeswap.tsx

--- src/bridges/Bridgeswap.tsx
+++ src/bridges/Bridgeswap.tsx
@@ -1,10 +1,9 @@
 import { LiFiWidget, WidgetConfig } from '@lifi/widget';
 
 const widgetConfig: WidgetConfig = {
-  integrator: 'krusty',
-  fromChain: 137,
-  fromToken: '0x386C126Edf2013A0f146BDe8dF1b67D93e2ACE90',
-   logoURI: 'https://raw.githubusercontent.com/Krustycoin0/swap/main/src/components/navbar/logoURI.jpg',
+  integrator: 'LSP20',
+  fromChain: 56,
+  fromToken: '0xe62a9bc6ede534e18dd2793dcaf5a2b6df112180',
   containerStyle: {
     border: '1px solid rgb(234, 234, 234)',
     borderRadius: '16px',
